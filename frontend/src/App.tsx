2f4dc27aae2366613a43b8391908536d9d69ac1d
Sushil <sushilrawat1720@gmail.com>
2023-07-14 01:26:13 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginSignup from "./components/organisms/LoginSignup";
 import VideoDetail from "./components/organisms/VideoDetail";
 function App() {
-    return (
-        <div className="text-white bg-dark font-gothic">
-            <Router>
-                {" "}
-                <Routes>
-                    <Route path="/" element={<Layout />} />
-                    <Route path="/auth" element={<LoginSignup />} />
-                    <Route path="/video/:id" element={<Layout />} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="text-white bg-dark font-gothic">
+      <Router>
+        {/* Someithing useless */}{" "}
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="/auth" element={<LoginSignup />} />
+          <Route path="/video/:id" element={<Layout />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
